925a4d33ce3e3af3eeeecc4ddbcaf7cbb855615a
Sumit Prasad <sprasa15@partner.jaguarlandrover.com>
2024-01-28 20:44:58 +0000

    Add comments & formatting code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # IndexedDB CRUD Operations using Vanilla JS
 A simple js application to perform CRUD operations on indexedDB.
 
-**NOTE:** This application is created only for demo purpose.
+**NOTE:** This application is created only for demo purpose. Serve the files using any server. E.g., python3 -m http.server
 
 ## View Demo [Click Here](https://whiskeystore-vanillajs.netlify.app/)
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { uid } from './uid.js';
 
 let db = null; 
 let objectStore = null;
-let DBOpenRequest = indexedDB.open('WhiskeyDB', 1);
+let DBOpenRequest = indexedDB.open('WhiskeyDB', 1); // db name, db version
 
 DBOpenRequest.addEventListener('error', err => {
     console.warn('An Error occurred opening the database', err);
@@ -11,6 +11,8 @@ DBOpenRequest.addEventListener('error', err => {
 DBOpenRequest.addEventListener('success', e => {
     db = e.target.result; 
     console.log('success', db);
+
+    // Populate the page with already present data in indexedDB whiskeyStore
     buildList();
 });
 
@@ -26,6 +28,11 @@ DBOpenRequest.addEventListener('upgradeneeded', e => {
     }
 });
 
+/**
+ * Creating method for creating transactions due to which we
+ * don't need to write the error handler for every transaction 
+ * if the action performed in the error handler is same
+ */
 function createTransaction(storeName, mode) {
     let tx = db.transaction(storeName, mode);
     tx.onerror = err => {
@@ -36,9 +43,9 @@ function createTransaction(storeName, mode) {
 
 const form = document.querySelector('form.whiskeyForm');
 const list = document.querySelector('ul.whiskeyList');
-const name = document.querySelector('#name');
-const country = document.querySelector('#country');
-const age = document.querySelector('#age');
+const nameInput = document.querySelector('#name');
+const countryInput = document.querySelector('#country');
+const ageInput = document.querySelector('#age');
 const owned = document.querySelector('#owned');
 
 const addBtn = document.querySelector('#addBtn');
@@ -57,164 +64,160 @@ resetBtn.addEventListener('click', resetForm);
 list.addEventListener('click', listItemClicked);
 
 function resetForm() {
-    name.value = '';
-    country.value = '';
-    age.value = '';
+    nameInput.value = '';
+    countryInput.value = '';
+    ageInput.value = '';
     owned.checked = false;
 }
 
 function buildList() {
-    let tx = createTransaction('whiskeyStore', 'readonly');
-    tx.oncomplete = e => {
-        console.log('TRANSACTION COMPLETE');
-    };
-    
-    let store = tx.objectStore('whiskeyStore');
-    let request = store.getAll();
-    
-    request.onsuccess = e => {
-        console.log('REQUEST COMPLETE, fetched all objects from object store');
-        const req = e.target;
-        const data = req.result;
-        
-        list.innerHTML = data
-        .map(current => {
-                            return `<li data-key=${current.id}><span>${current.name} </span> from ${current.country}<br> ${current.age} years old`;
-                        })
-                        .join('\n');
-                    };
-                    
-                    request.onerror = err => {
-                        console.warn('REQUEST FAILED, An error occurred getting all objects from object store', err);
-    }
+  let tx = createTransaction('whiskeyStore', 'readonly');
+  tx.oncomplete = e => {
+    console.log('TRANSACTION COMPLETE');
+  };
+  
+  let store = tx.objectStore('whiskeyStore');
+  let request = store.getAll();
+  
+  request.onsuccess = (e) => {
+    console.log('REQUEST COMPLETE, fetched all objects from object store');
+    const req = e.target;
+    const data = req.result;
+
+    list.innerHTML = data
+      .map((current) => {
+        return `<li data-key=${current.id}><span>${current.name} </span> from ${current.country}<br> ${current.age} years old`;
+      })
+      .join('\n');
+  };
+                  
+  request.onerror = err => {
+    console.warn('REQUEST FAILED, An error occurred getting all objects from object store', err);
+  }
 }
 
 function addWhiskey() {
-    if(name.value.trim() === '' || country.value.trim() === '' || age.value.trim() === '') { 
-        alert('Please fill all the form fields');
-        return;
-    }
+  if(nameInput.value.trim() === '' || countryInput.value.trim() === '' || ageInput.value.trim() === '') { 
+      alert('Please fill all the form fields');
+      return;
+  }
     
-    if(isNaN(age.value)) {
-        alert('Please enter a number for years old field');
-        return;
-    }
+  if(isNaN(ageInput.value)) {
+      alert('Please enter a number for years old field');
+      return;
+  }
 
-    let whiskey = {
-        id: uid(),
-        name: name.value,
-        country: country.value,
-        age: parseInt(age.value),
-        owned: owned.checked
-    }
+  let whiskey = {
+      id: uid(),
+      name: nameInput.value,
+      country: countryInput.value,
+      age: parseInt(ageInput.value),
+      owned: owned.checked
+  }
 
-    let tx = createTransaction('whiskeyStore', 'readwrite');
-    tx.oncomplete = e => {
-        console.log('TRANSACTION COMPLETE');
-        buildList();
-        resetForm();
-    }
+  let tx = createTransaction('whiskeyStore', 'readwrite');
+  tx.oncomplete = e => {
+    console.log('TRANSACTION COMPLETE');
+    buildList();
+    resetForm();
+  }
 
-    let store = tx.objectStore('whiskeyStore');
-    let request = store.add(whiskey);
-    
-    request.onsuccess = e => {
-        console.log('REQUEST COMPLETE, object was added successfully to object store');
-    };
+  let store = tx.objectStore('whiskeyStore');
+  let request = store.add(whiskey);
+  
+  request.onsuccess = e => {
+    console.log('REQUEST COMPLETE, object was added successfully to object store');
+  };
 
-    request.onerror = err => {
-        console.warn('REQUEST FAILED, An error occurred adding the object', err);
-    }
+  request.onerror = err => {
+    console.warn('REQUEST FAILED, An error occurred adding the object', err);
+  }
 }
-
-
 
 function listItemClicked(e) {
-    const li = e.target.closest('[data-key]');
-    const id = li.getAttribute('data-key');
-    getSingleWhiskey(id, (whiskey) => {
-        name.value = whiskey.name;
-        country.value = whiskey.country;
-        age.value = whiskey.age.toString();
-        owned.checked = whiskey.owned;
-        form.setAttribute('data-key', whiskey.id);
-    });
+  const li = e.target.closest('[data-key]');
+  const id = li.getAttribute('data-key');
+  getSingleWhiskey(id, (whiskey) => {
+    nameInput.value = whiskey.name;
+    countryInput.value = whiskey.country;
+    ageInput.value = whiskey.age.toString();
+    owned.checked = whiskey.owned;
+    form.setAttribute('data-key', whiskey.id);
+  });
 }
 
-
 function updateWhiskey() {
-    const id = form.getAttribute('data-key');
-    if(!id) return;
-    const whiskey = {
-        id,
-        name: name.value,
-        country: country.value,
-        age: parseInt(age.value),
-        owned: owned.checked
-    }
-    
-    let tx = createTransaction('whiskeyStore', 'readwrite');
-    tx.oncomplete = e => {
-        console.log('TRANSACTION COMPLETE');
-        buildList();
-    };
+  const id = form.getAttribute('data-key');
+  if(!id) return;
+  const whiskey = {
+    id,
+    name: nameInput.value,
+    country: countryInput.value,
+    age: parseInt(ageInput.value),
+    owned: owned.checked
+  }
+  
+  let tx = createTransaction('whiskeyStore', 'readwrite');
+  tx.oncomplete = e => {
+    console.log('TRANSACTION COMPLETE');
+    buildList();
+  };
 
-    let store = tx.objectStore('whiskeyStore');
-    let request = store.put(whiskey);
+  let store = tx.objectStore('whiskeyStore');
+  let request = store.put(whiskey);
 
-    request.onsuccess = e => {
-        console.log('REQUEST COMPLETE, Successfully updated the selected object in the object store');
-        const req = e.target;
-        const data = req.result;
-    };
+  request.onsuccess = e => {
+    console.log('REQUEST COMPLETE, Successfully updated the selected object in the object store');
+    const req = e.target;
+    const data = req.result;
+  };
 
-    request.onerror = err => {
-        console.warn('REQUEST FAILED, An error occurred updating the selected object in the object store', err);
-    }
-
+  request.onerror = err => {
+    console.warn('REQUEST FAILED, An error occurred updating the selected object in the object store', err);
+  }
 }
 
 function deleteWhiskey() {
-    const id = form.getAttribute('data-key');
-    if(!id) return;
-    let tx = createTransaction('whiskeyStore', 'readwrite');
-    tx.oncomplete = e =>{
-        console.log('TRANSACTION COMPLETE');
-        resetForm();
-        buildList();
-    };
+  const id = form.getAttribute('data-key');
+  if(!id) return;
+  let tx = createTransaction('whiskeyStore', 'readwrite');
+  tx.oncomplete = e =>{
+    console.log('TRANSACTION COMPLETE');
+    resetForm();
+    buildList();
+  };
 
-    let store = tx.objectStore('whiskeyStore');
-    let request = store.delete(id);
+  let store = tx.objectStore('whiskeyStore');
+  let request = store.delete(id);
 
-    request.onsuccess = e => {
-        console.log('REQUEST COMPLETE, Successfully deleted the selected object from the object store');
-    };
+  request.onsuccess = e => {
+    console.log('REQUEST COMPLETE, Successfully deleted the selected object from the object store');
+  };
 
-    request.onerror = err => {
-        console.warn('REQUEST FAILED, Error occurred while deleting a single object from the object store');
-    }
+  request.onerror = err => {
+    console.warn('REQUEST FAILED, Error occurred while deleting a single object from the object store');
+  }
 }
 
 function getSingleWhiskey(id, callback) {
-    let whiskey = null; 
-    let tx = createTransaction('whiskeyStore', 'readonly');
-    tx.oncomplete = e => {
-        console.log('TRANSACTION COMPLETE');
-        if(callback) callback(whiskey);
-    };
+  let whiskey = null; 
+  let tx = createTransaction('whiskeyStore', 'readonly');
+  tx.oncomplete = e => {
+    console.log('TRANSACTION COMPLETE');
+    if(callback) callback(whiskey);
+  };
 
-    let store = tx.objectStore('whiskeyStore');
-    let request = store.get(id);
+  let store = tx.objectStore('whiskeyStore');
+  let request = store.get(id);
 
-    request.onsuccess = e => {
-        console.log('REQUEST COMPLETE, successfully fetched a single whiskey from the object store');
-        const req = e.target;
-        const data = req.result;
-        whiskey = data;
-    };
+  request.onsuccess = e => {
+    console.log('REQUEST COMPLETE, successfully fetched a single whiskey from the object store');
+    const req = e.target;
+    const data = req.result;
+    whiskey = data;
+  };
 
-    request.onerror = err => {
-        console.warn('REQUEST FAILED, An error occurred fetching a single whiskey from the object store', err);
-    };
+  request.onerror = err => {
+    console.warn('REQUEST FAILED, An error occurred fetching a single whiskey from the object store', err);
+  };
 }
